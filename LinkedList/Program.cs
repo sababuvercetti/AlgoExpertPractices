965b6ae22fd019e4f13670290755e3f35a75f756
Samuel Baraka <sababuvercetti@gmail.com>
2022-09-29 11:17:57 +0300

    created linkedlist project

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Create the linked list
+            string[] words = { "Hello", "There", "My Name", "Is", "Sam" };
+            LinkedList<String> sentence = new LinkedList<string>(words);
+            Display(sentence, "The linked list values:");
+
+            /// Get the number of items in the linkedlist
+            Console.Write("LinkedList Items:"+sentence.Count());
+        }
+
+        private static void Display(LinkedList<string> words, string test)
+        {
+            Console.WriteLine(test);
+            foreach (string word in words)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
